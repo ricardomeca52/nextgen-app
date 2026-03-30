5a6d06ac967ce4302ac4dcd102dd311ffa741f0e
Ricardomeca04 <a2022139194@alumni.iscac.pt>
2026-03-30 17:38:20 +0100

    Primeiro envio do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Indica ao Tailwind onde estão os teus componentes para ele aplicar os estilos
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
